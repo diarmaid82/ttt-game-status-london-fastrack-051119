2e83ddb4618c58352d388f7dcf3d2832b7c6f7fb
comical-closure-6767 <diarmaid.murtagh@gmail.com>
2019-05-12 12:18:40 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,20 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # middle row
+  [6,7,8], # bottom row
+  [0,4,8], # left diagonal
+  [2,4,6], # right diagonal
+  [0,3,6], # first column
+  [1,4,7], # second column
+  [2,5,8] # third column
+  ]
+  
+  def #won 
+  for each #WIN_COMBINATIONS
+  
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
